c19346e248b520ac5903294af964940457aae0b3
narrowtux <narrow.m@gmail.com>
2011-11-06 00:50:33 +0100

    Only execute binding when on the main screen. (Keybindings report keypresses for all screens).

--- src/main/java/org/getspout/testplugin/TestKeyBinding.java
+++ src/main/java/org/getspout/testplugin/TestKeyBinding.java
@@ -5,11 +5,13 @@ import org.bukkit.Material;
 import org.getspout.spoutapi.SpoutManager;
 import org.getspout.spoutapi.event.input.KeyBindingEvent;
 import org.getspout.spoutapi.keyboard.BindingExecutionDelegate;
+import org.getspout.spoutapi.gui.ScreenType;
 
 public class TestKeyBinding implements BindingExecutionDelegate {
 	
 	
 	public void keyPressed(KeyBindingEvent event) {
+		if (event.getScreen() != ScreenType.GAME_SCREEN) return;
 		if (event.getBinding().getId() == "TopCatKey") {
 			SpoutManager.getAppearanceManager().setGlobalSkin(event.getPlayer(), "http://s3.amazonaws.com/MinecraftSkins/Top_Cat.png");
 		} else if (event.getBinding().getId() == "PartyTimeKey") {
